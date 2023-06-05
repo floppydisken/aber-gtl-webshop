ce549c126de50aff783ec89ff50b4275ada7e6f7
Bjarke Sporring <work.bjarke@gmail.com>
2023-06-05 15:00:48 +0200

    Add initial Voucher creation

--- Webshop.Catalog.Solution/Webshop.Order.Application/Features/CreateVoucherCommandHandler.cs
+++ Webshop.Catalog.Solution/Webshop.Order.Application/Features/CreateVoucherCommandHandler.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Webshop.Application.Contracts;
 using Webshop.Domain.Common;
 using Webshop.Order.Application.Abstractions;
+using Webshop.Order.Domain.ValueObjects;
 using Webshop.Order.Persistence.Abstractions;
 
 namespace Webshop.Order.Application.Features;
@@ -14,9 +15,34 @@ public class CreateVoucherCommandHandler : ICreateVoucherCommandHandler
     {
         this.voucherRepository = voucherRepository;
     }
-    
-    public Task<Result> Handle(CreateVoucherCommand request, CancellationToken cancellationToken)
+
+    public async Task<Result> Handle(CreateVoucherCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var discountResult = FluentVogen
+            .UseMapper(() => Discount.From(request.Amount))
+            .UseError((e) =>
+                Errors.General.ValueOutOfRange(nameof(request.Amount), Discount.Min.Value, Discount.Max.Value,
+                    e.Message))
+            .Run();
+
+        if (discountResult.Failure)
+        {
+            return discountResult;
+        }
+
+        try
+        {
+            await voucherRepository.CreateAsync(new()
+            {
+                Code = VoucherCode.From(request.Code),
+                Discount = discountResult.UnwrapOr(Discount.FromOrBoundary(request.Amount))
+            });
+
+            return Result.Ok();
+        }
+        catch (Exception e)
+        {
+            return Result.Fail(e.Message);
+        }
     }
 }
