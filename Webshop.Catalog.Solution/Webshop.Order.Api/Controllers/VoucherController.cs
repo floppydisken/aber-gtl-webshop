c92bd326d1653db48dff970969108506f9ea51a9
AronHetLam <aronhetlam@gmail.com>
2023-06-06 11:16:15 +0200

    Add voucher endpoints

--- Webshop.Catalog.Solution/Webshop.Order.Api/Controllers/VoucherController.cs
+++ Webshop.Catalog.Solution/Webshop.Order.Api/Controllers/VoucherController.cs
@@ -1,9 +1,50 @@
-using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Webshop.Api;
+using Webshop.Application.Contracts;
+using Webshop.Order.Application.Abstractions;
 
 namespace Webshop.Order.Api.Controllers;
 
 [Route("api/vouchers")]
 public class VoucherController : WebshopController
 {
+    private readonly IDispatcher dispatcher;
+
+    public VoucherController(IDispatcher dispatcher)
+    {
+        this.dispatcher = dispatcher;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateVoucherAsync([FromBody] CreateVoucherCommand request)
+    {
+        var result = await dispatcher.Dispatch(request);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{code}")]
+    public async Task<IActionResult> DeleteVoucherAsync(string code)
+    {
+        var result = await dispatcher.Dispatch(new DeleteVoucherCommand {Code = code});
+        
+        return result.ToResponse();
+    }
+
+    [HttpPost("storewide")]
+    public async Task<IActionResult> SetStorewideDiscountAsync([FromBody] SetStoreWideDiscountCommand request)
+    {
+        var result = await dispatcher.Dispatch(request);
+
+        return Ok(result);
+    }
+
+    [HttpGet("storewide")]
+    public async Task<IActionResult> GetStorewideDiscountAsync(string code)
+    {
+        var result = await dispatcher.Dispatch(new DeleteVoucherCommand {Code = code});
+        
+        return result.ToResponse();
+    }
 }
