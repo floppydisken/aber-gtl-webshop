b017e001093dbd2740c8512b817988f0288bf3a3
AronHetLam <aronhetlam@gmail.com>
2023-06-06 15:45:44 +0200

    Fix order api config

--- Webshop.Catalog.Solution/Webshop.Order.Application/ServiceCollectionExtensions.cs
+++ Webshop.Catalog.Solution/Webshop.Order.Application/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Webshop.Application;
 using Webshop.Order.Persistence;
@@ -8,11 +9,14 @@ namespace Webshop.Order.Application;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddOrderServices(this IServiceCollection services)
+    public static IServiceCollection AddOrderServices(this IServiceCollection services, IConfiguration configuration)
     {
+        
         services.AddOrderMongoPersistence();
         services.AddHttpClient();
-        services.AddScoped<CatalogClient>(provider => new CatalogClient(provider.GetRequiredService<HttpClient>(), new()));
+        services.AddScoped<CatalogClient>(provider => new CatalogClient(provider.GetRequiredService<HttpClient>(), new() {
+            Uri = new Uri(configuration.GetValue<string>("Catalog:Uri"))
+        }));
         services.AddScoped<CustomerClient>(provider => new CustomerClient(provider.GetRequiredService<HttpClient>(), new()));
         services.AddPaymentClient();
         services.AddDispatcher(typeof(ServiceCollectionExtensions).Assembly);
